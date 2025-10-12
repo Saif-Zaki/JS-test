888f0c3f1a4c64043b2e34519b99e75bcdf4863e
Saif-Zaki <saifmahmoudzaki@gmail.com>
2025-10-12 16:07:13 +0300

    Assignment1

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function checkIfAllowed(){
-    let age = Number(document.getElementById("test").value)
-    let color = document.getElementById("colorInput").value
-    if(age >= 16){
-        document.getElementById("result").innerHTML = "allowed";
-    }
-    else {
-        document.getElementById("result").innerHTML = "not allowed";
-    }
+let text = "We are specialized in sourcing and providing high-qualitybuilding materials to meet the diverse needs of constructionprojects, ensuring excellence in every structure we contribute to.";
+document.getElementById("t1").innerHTML=text;
+document.getElementById("t2").innerHTML=text;
+document.getElementById("t3").innerHTML= text;
+
+function Scroll(SectionId){
+    document.getElementById(SectionId).scrollIntoView();
 }
